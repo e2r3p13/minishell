74eb0ea82c0fd3dc07af65754842428bf1c7845b
Placard <loic.falkau@hotmail.fr>
2020-01-21 03:29:46 +0100

    Filal script version + cd test

--- srcs/gdd.sh
+++ srcs/gdd.sh
@@ -9,37 +9,27 @@ C='\033[0;36m'
 
 git status &> /tmp/.minishell_git_logs
 
-while read line
-do
-	if [[ $line == *"not a git repository"* ]]
-	then
+while read line; do
+	if [[ $line == *"not a git repository"* ]]; then
 		let "id = 0"
-	elif [[ $line == "Untracked files:" || $line == "Changes not staged for commit:" ]]
-	then
+	elif [[ $line == "Untracked files:" || $line == "Changes not staged for commit:" ]]; then
 		let "id = id + 1000"
-	elif [[ $line == "Changes to be committed:" ]]
-	then
+	elif [[ $line == "Changes to be committed:" ]]; then
 		let "id = id + 100"
-	elif [[ $line == *"nothing to commit, working tree clean"* ]]
-	then
+	elif [[ $line == *"nothing to commit, working tree clean"* ]]; then
 		let "id = id + 1"
-	elif [[ $line == *"Your branch is ahead of"* ]]
-	then
+	elif [[ $line == *"Your branch is ahead of"* ]]; then
 		let "id = id + 10"
 	fi
 done < <(cat /tmp/.minishell_git_logs)
 
-if [ $id -eq 0 ]
-then
+if [ $id -eq 0 ]; then
 	printf "${C} » ${W}"
-elif [ $id -lt 10 ]
-then
+elif [ $id -lt 10 ]; then
 	printf "${Y} » ${W}"
-elif [ $id -lt 100 ]
-then
+elif [ $id -lt 100 ]; then
 	printf "${P} » ${W}"
-elif [ $id -lt 1000 ]
-then
+elif [ $id -lt 1000 ]; then
 	printf "${G} » ${W}"
 else
 	printf "${R} » ${W}"
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 17:37:37 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/01/21 02:57:13 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/01/21 03:27:11 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	minishell(char **env, char *exec_path)
 	prompt(env, exec_path);
 	while (get_next_line(0, &line))
 	{
-		prompt(env, exec_path);
 		chdir(line);
+		prompt(env, exec_path);
 		save_command(line, "/tmp/history");
 		free(line);
 	}
