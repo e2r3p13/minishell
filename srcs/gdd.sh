10de5f624f9bf9351cbd917936ba4543b318132d
Placard <loic.falkau@hotmail.fr>
2020-01-21 02:55:28 +0100

    Cool colored prompt

--- srcs/gdd.sh
+++ srcs/gdd.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
-YELLOW='\033[0;93m'
-GREEN='\033[0;92m'
-RED='\033[0;91m'
-CYAN='\033[0;36m'
-WHITE='\033[m'
+W='\033[0;00m'
+R='\033[0;91m'
+Y='\033[0;93m'
+G='\033[0;92m'
+P='\033[0;95m'
+C='\033[0;36m'
 
 git status &> /tmp/.minishell_git_logs
 
-cat /tmp/.minishell_git_logs | while read line ; do
+while read line
+do
 	if [[ $line == *"not a git repository"* ]]
 	then
-		printf "${YELLOW} » ${WHITE}"
-		rm /tmp/.minishell_git_logs
-		exit 0
+		id=0
+	elif [[ $line == "Untracked files:" || $line == "Changes not staged for commit:" ]]
+	then
+		id=1
+	elif [[ $line == "Changes to be committed:" ]]
+	then
+		id=2
+	elif [[ $line == *"Your branch is ahead of"* ]]
+	then
+		id=3
 	elif [[ $line == *"nothing to commit, working tree clean"* ]]
 	then
-		printf "${GREEN} » ${WHITE}"
-		rm /tmp/.minishell_git_logs
-		exit 0
-	elif [[ $line == *"Untracked files:"* || $line == *"Changes not staged for commit:"* ]]
-	then
-		printf "${RED} » ${WHITE}"
-		rm /tmp/.minishell_git_logs
-		exit 0
+		id=4
 	fi
-done
+done < <(cat /tmp/.minishell_git_logs)
+
+case $id in
+	0)
+		printf "${C} » ${W}";;
+	1)
+		printf "${R} » ${W}";;
+	2)
+		printf "${G} » ${W}";;
+	3)
+		printf "${P} » ${W}";;
+	4)
+		printf "${Y} » ${W}";;
+esac
+
+rm -f /tmp/.minishell_git_logs
