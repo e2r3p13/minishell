d0131203f00e897d4f04f5fa6854ef0468a70988
Kali Live user <sablayrolles.antoine@gmail.com>
2020-03-18 14:58:13 +0000

    pls no roll back

--- srcs/lex_utils.c
+++ srcs/lex_utils.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "minishell.h"
+#include "libft.h"
 
 t_list	*ft_list_new(void *content)
 {
@@ -29,10 +30,35 @@ char	*ft_append(char *str, char c)
 		if (!(s = (char *)malloc(sizeof(char) * ft_strlen(str) + 1)))
 			return (NULL);
 		while(str[i])
-			s[i] = str[i++];
+		{
+			s[i] = str[i];
+			i++;
+		}
 		free(str);
 	}
 	s[i++] = c;
 	s[i] = 0;
 	return (s);
+}
+
+char    *ft_strndup(const char *s1, size_t n)
+{
+        size_t  len;
+        char    *s2;
+
+        len = ft_strlen(s1);
+	if (len < n)
+	{
+        	if (!(s2 = (char *)malloc(sizeof(char) * (len + 1))))
+                	return (NULL);
+        	ft_strlcpy(s2, s1, n + 1);
+	}
+	else
+	{
+		if (!(s2 = (char *)malloc(sizeof(char) * (n + 1))))
+                	return (NULL);
+        	ft_strlcpy(s2, s1, n);
+		s2[n] = 0;
+        }
+	return (s2);
 }
--- srcs/lexer.c
+++ srcs/lexer.c
@@ -3,39 +3,111 @@
 #include "minishell.h"
 #include <unistd.h>
 
-static int	get_quoted(char *str, t_list *cur)
+static int	get_var(char *str, t_list **cur)
+{
+	int	i;
+
+	i = 1;
+	while (str[i] && ft_isalnum(str[i]))
+		i++;
+	(*cur)->token = VAR;
+	(*cur)->raw = ft_strndup(str, i);
+	return (i);
+}
+
+static int	get_word(char *str, t_list **cur)
 {
 	int	i;
 
 	i = 0;
+	while (str[i] && ft_isalnum(str[i]))
+		i++;
+	(*cur)->token = WORD;
+	(*cur)->raw = ft_strndup(str, i);
+	return (i);
+}
+
+static int	get_anti_quote(char *str, t_list *cur)
+{
+	int	i;
+
+	i = 1;
+	while (str[i] && ft_isalnum(str[i]))
+		i++;
+	cur->token = QUOTED;
+	cur->raw = ft_strndup(str, i);
+	return (i);
+}
+
+static int	get_single_quote(char *str, t_list *cur)
+{
+	int	len;
+
+	len = ft_strchr(str + 1, 39) - str;
+	if (len > 0 && (ft_strchr(str + 1, 39) < ft_strchr(str + 1, 10)))//lacks EOT
+		cur->raw = ft_strndup(str, len);
+	else
+	{
+		len = 1;
+		while (str[len] && str[len] != 4 && str[len] != 39)
+			len++;
+		cur->raw = ft_strndup(str, len);
+	}
+	cur->token = QUOTED;
+	return (len);
+}
+
+static int	get_double_quote(char *str, t_list **cur)
+{
+	int	len;
+	int	i;
+
+	len = ft_strchr(str + 1, '"') - str;
+	if (len > 0 && (ft_strchr(str + 1, '"') < ft_strchr(str + 1, 10)))
+		(*cur)->raw = ft_strndup(str, len);
+	else
+	{
+		len = 1;
+		while (str[len] && str[len] != 4 && str[len] != '"')
+			len++;
+		(*cur)->raw = ft_strndup(str, len);
+	}
+	(*cur)->token = QUOTED;
+	i = ft_strchr(str, '$') - str;
+	while (i > 0 && i < len)//euh... not so sure
+	{
+		(*cur)->next = ft_list_new(NULL);
+		*cur = (*cur)->next;
+		i += get_var(str + i, cur);
+		i += ft_strchr(str + i, '$') - str;
+	}
+	return (len);
+}
+
+static int	get_quoted(char *str, t_list **cur)
+{
+	int	i;
+
+	i = 0;
+	if (*str == 39)
+		return (get_single_quote(str, *cur));
+	else if (*str == '"')
+		return (get_double_quote(str, cur));
+	else if (*str == 92)
+		return (get_anti_quote(str, *cur));
 	return (i);
 }
 
 static int	get_comment(char *str, t_list *cur)
 {
-	int	i;
 	int	len;
 
-	i = 0;
 	len = 0;
 	while (str[len] && str[len] != 10)//add carriage ret?
 		len++;
-	cur->raw = ft_strndup(str, len);//ft_strndup does not exist
-	i = ft_strchr(str, '$') - str;
-	while (i < len)//euh... not so sure
-	{
-		i += get_var(str + i, cur);
-		i += ft_strchr(str + i, '$') - str;
-	}
-	return (i);
-}
-
-static int	get_var(char *str, t_list *cur)
-{
-	int	i;
-
-	i = 0;
-	return (i);
+	cur->token = COMMENT;
+	cur->raw = ft_strndup(str, len);
+	return (len);
 }
 
 t_list		*lexer(char *str)
@@ -49,83 +121,27 @@ t_list		*lexer(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == ' ')
+		if (cur->token)
 		{
-			if (cur->token)
-			{
-				cur->next = ft_list_new(NULL);
-				cur = cur->next;
-			}
-		}
-		else if (str[i] == 4)
-		{
-			if (cur->token)
-			{
-				cur->next = ft_list_new(NULL);
-				cur = cur->next;
-			}
-			cur->token = EOI;
-		}
-		else if (str[i] == '\n' || str[i] == ';')
-		{
-			if (cur->token)
-			{
-				cur->next = ft_list_new(NULL);
-				cur = cur->next;
-			}
-			cur->token = NEWLINE;
-		}
-		else if (ft_isinset("+-*/%=!", str[i]) && cur->token != OPERATOR)
-		{
-			if (cur->token)
-			{
-				cur->next = ft_list_new(NULL);
-				cur = cur->next;
-			}
-			cur->token = OPERATOR;
-		}
-		else if (ft_isinset("\\\'\"", cur->raw[0]))
-		{
-/*			i += get_quoted(str + i, cur);
-			while (cur->next)
-				cur = cur->next;
-*/			if (cur->token == QUOTED)
-				cur->raw = append(cur->raw, str[i]);
-			else
-			{
-				cur->next = ft_list_new(NULL);
-				cur = cur->next;
-				cur->token = QUOTED;
-			}
-		}
-		else if (str[i] == '$')
-		{
-//			i += get_var(str + i, cur);
-			if (cur->token)
-			{
-				cur->next = ft_list_new(NULL);
-				cur = cur->next;
-			}
-			cur->token = VAR;
-		}
-		else if (str[i] == '#')
-		{
-//			i += get_comment(str + i, cur);
-			cur->next = ft_list_new("");
+			cur->next = ft_list_new(NULL);
 			cur = cur->next;
-			cur->token = COMMENT;
-			while (str[i + 1] && str[i + 1] != 4 && str[i + 1] != '\n')
-				cur->raw = append(cur->raw, str[i++]);
 		}
-		else if (!cur->token)
-		{
-			cur->token = WORD;
-		}
-		cur->raw = ft_append(cur->raw, str[i++]);
+		if (str[i] == ' ')
+			i++;
+		else if (str[i] == 4)//might not be necessary
+			cur->token = EOI;
+		else if (str[i] == '\n' || str[i] == ';')
+			cur->token = NEWLINE;
+		else if (ft_isinset("+-*/%=!", str[i]) && cur->token != OPERATOR)
+			cur->token = OPERATOR;
+		else if (ft_isinset("\\\'\"", cur->raw[0]))
+			i += get_quoted(str + i, &cur);
+		else if (str[i] == '$')
+			i += get_var(str + i, &cur);
+		else if (str[i] == '#')
+			i += get_comment(str + i, cur);
+		else
+			i += get_word(str + i, &cur);
 	}
-	cur = head->next;
-	if (head->raw)
-		free(head->raw);
-	free(head);
-	return (cur);
+	return (head);
 }
