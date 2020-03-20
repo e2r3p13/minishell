0d1d49e87844790e1f966d8997730da3c09579e7
Loïc Buckwell <lo.buckwell@gmail.com>
2020-03-20 18:59:44 +0100

    clean code pt. 1

--- srcs/get_command.c
+++ srcs/get_command.c
@@ -6,29 +6,30 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/19 00:35:57 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/03/20 18:21:05 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/03/20 18:59:13 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "keys.h"
 
-static void handle_return(t_cmd *cmd)
+static void cmd_return(t_cmd *cmd)
 {
 	char *next_line;
 
-	save_cmd(cmd->raw, HISTORY_PATH);
+	//Multiline, if line is trailed by '\'
 	if (cmd->raw[cmd->len - 1] == '\\')
 	{
-		write(1, "\n\033[0;36m» \033[0;00m", ft_strlen("\n\033[0;36m» \033[0;00m"));
+		write(1, "\n\033[0;36m» \033[0;00m", 18);
 		next_line = get_cmd();
 		join_commands(cmd, next_line);
 	}
 }
 
-static void handle_escape(t_cmd *cmd, char *buf)
+static void cmd_escape(t_cmd *cmd, char *buf)
 {
-	if (buf[1] == 91)
+	//Handle arrows
+	if (buf[1] == '[')
 	{
 		if (buf[2] == ESC_KEY_UP)
 			printf("");
@@ -53,7 +54,7 @@ static void handle_escape(t_cmd *cmd, char *buf)
 	cmd->len--;
 }
 
-static void handle_backspace(t_cmd *cmd)
+static void cmd_backspace(t_cmd *cmd)
 {
 	int cpos_diff;
 
@@ -68,7 +69,7 @@ static void handle_backspace(t_cmd *cmd)
 	}
 }
 
-static void handle_ctrl_d(t_cmd *cmd)
+static void cmd_ctrld(t_cmd *cmd)
 {
 	if (cmd->len == 0)
 	{
@@ -78,11 +79,11 @@ static void handle_ctrl_d(t_cmd *cmd)
 	{
 		write(0, "\033[C", ft_strlen("\033[C"));
 		cmd->cpos++;
-		handle_backspace(cmd);
+		cmd_backspace(cmd);
 	}
 }
 
-static void	handle_characters(t_cmd *cmd, char c)
+static void	cmd_character(t_cmd *cmd, char c)
 {
 	int cpos_diff;
 
@@ -96,29 +97,29 @@ static void	handle_characters(t_cmd *cmd, char c)
 	}
 }
 
-char *get_cmd()
+char		*get_cmd()
 {
-	t_cmd			*cmd;
-	char			buf[4];
+	t_cmd	*cmd;
+	char	buf[4];
 
 	cmd = new_cmd();
-	while (1)
+	while (true)
 	{
 		ft_memset(buf, 0, 4);
 		read(0, &buf, 4);
 		if (buf[0] == ESCAPE_KEY)
-			handle_escape(cmd, buf);
+			cmd_escape(cmd, buf);
 		else if (buf[0] == RETURN_KEY)
 		{
-			handle_return(cmd);
+			cmd_return(cmd);
 			break;
 		}
 		else if (buf[0] == BACKSPACE_KEY)
-			handle_backspace(cmd);
+			cmd_backspace(cmd);
 		else if (buf[0] == CTRL_D_KEY)
-			handle_ctrl_d(cmd);
+			cmd_ctrld(cmd);
 		else
-			handle_characters(cmd, buf[0]);
+			cmd_character(cmd, buf[0]);
 	}
 	return (cmd->raw);
 }
