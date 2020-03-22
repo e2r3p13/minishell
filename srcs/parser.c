9bdcdfa781d8c0dcebdec05a4b4da07f39193b62
Kali Live user <sablayrolles.antoine@gmail.com>
2020-03-22 02:22:07 +0000

    beta parser

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
-#include "token.h"
-#include "libfr.h"
+#include "tokens.h"
+#include "libft.h"
 
 static int	get_cmd_length(t_lex_lst *head)
 {
@@ -22,51 +22,51 @@ static char	**create_simple_cmd(t_lex_lst **head)
 
 	if (!(*head))
 		return (NULL);
-	i = get_cmd_length(head);
+	i = get_cmd_length(*head);
 	while (*head && (*head)->token == WORD)
 	{
 		cmd[i++] = (*head)->raw;
 		*head = (*head)->next;
 	}
-	smp_cmd[i] = 0;
+	cmd[i] = 0;
 	return (cmd);
 }
 		
 
-static s_rdct	*create_redirect_cmd(void *left, t_lex_lst **cur)
+static t_rdct	*create_redirect_cmd(void *left, t_lex_lst **cur)
 {
-	s_rdct	lst_head;
+	t_rdct	*lst_head;
 
+	if (!(lst_head = (t_rdct *)malloc(sizeof(t_rdct))))
+		return (NULL);
 	lst_head->left = left;
 	if (!(*cur) || (*cur)->token != REDIRECT)
 		lst_head->type = 0;
-	else if (!ft_strncmp(cur->raw, "||", 3))
+	else if (!ft_strncmp((*cur)->raw, "||", 3))
 		lst_head->type = PIPE;
-	else if (!ft_strncmp(cur->raw,"<", 2))
+	else if (!ft_strncmp((*cur)->raw,"<", 2))
 		lst_head->type = LESS;
-	else if (!ft_strncmp(cur->raw, "<<", 3))
+	else if (!ft_strncmp((*cur)->raw, "<<", 3))
 		lst_head->type = DLESS;
-	else if (!ft_strncmp(cur->raw, ">", 2))
+	else if (!ft_strncmp((*cur)->raw, ">", 2))
 		lst_head->type = GREAT;
-	else if (!ft_strncmp(cur->raw, ">>", 3))
+	else if (!ft_strncmp((*cur)->raw, ">>", 3))
 		lst_head->type = DGREAT;
 	if (*cur)
 		*cur = (*cur)->next;
 	lst_head->right = create_simple_cmd(cur);
-	return (&lst_head);
+	return (lst_head);
 }
 
-static s_rdct	*parser(t_lex_lst *cur)
+t_rdct		*parser(t_lex_lst *cur)
 {
 	void		*prs_head;
-	t_lex_lst	*cur;
 	char		**cmd;
 
-	cur = head;
 	prs_head = NULL;
 	cmd = create_simple_cmd(&cur);
 	if (!cur)
-		return (create_redirect_cmd(cmd, cur));
+		return (create_redirect_cmd(cmd, NULL));
 	while (cur && cur->token == REDIRECT)
 	{
 		if (!prs_head)
