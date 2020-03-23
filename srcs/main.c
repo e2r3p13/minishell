d28f4abf898008ea6b072ba2434ca4218d2403bd
Loïc Buckwell <lo.buckwell@gmail.com>
2020-03-23 15:33:10 +0100

    minor fixes

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/12 15:13:41 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/03/23 15:16:48 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/03/23 15:31:47 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ int	main(int ac, char **av, char **env)
 	av[ac] = NULL;
 	signal(SIGINT, ctrlc_handler);
 	signal(SIGTERM, ctrld_handler);
-	enable_raw_mode();
 	return (minishell(env));
 }
