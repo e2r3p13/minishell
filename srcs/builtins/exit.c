b2464860bdb8f3fb7167b768531786cc4518a361
Loïc Buckwell <lo.buckwell@gmail.com>
2020-03-26 11:55:42 +0100

    reset term settings at exit

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,15 +6,18 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 16:46:42 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/03/25 12:39:06 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/03/26 11:54:45 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+extern struct termios g_save;
+
 int ms_exit(int ac, char **av, char **env)
 {
 	if (av != env)
 		av[ac] = NULL;
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_save);
 	exit(0);
 }
