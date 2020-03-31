e722c673aeca41f009302edeaad3eb76dea4508d
Loïc Buckwell <lo.buckwell@gmail.com>
2020-03-31 18:40:31 +0200

    handle tilde in a more generic way

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 16:44:15 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/03/25 12:11:47 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/03/31 18:39:51 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 int ms_cd(int ac, char **av, char **env)
 {
-	char *path;
+	char	*path;
+	t_bool	is_allocated;
 
 	av[ac] = NULL;
+	is_allocated = false;
 	if ((path = av[1]) == NULL)
 		path = get_env_var("HOME=", env);
+	if (*path == '~')
+		if ((path = ft_strjoin(get_env_var("HOME=", env), path + 1)))
+			is_allocated = true;
 	if (path == NULL || chdir(path) == -1)
 	{
 		write(1, "cd: no such file or directory: ", 31);
@@ -26,5 +31,7 @@ int ms_cd(int ac, char **av, char **env)
 		write(1, "\n", 1);
 		return (1);
 	}
+	if (path && is_allocated)
+		free(path);
 	return (0);
 }
