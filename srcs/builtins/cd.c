6e90680a95b772df17f04a167b379d9145375ce1
Loïc Buckwell <lo.buckwell@gmail.com>
2020-04-05 13:54:56 +0200

    Protect cd against HOME unset

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,32 +6,54 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 16:44:15 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/04/03 15:59:28 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/04/05 13:54:42 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int ms_cd(int ac, char **av, t_env *env)
+static char	*set_path_with_home(char *arg, t_env *env, t_bool *allocated)
+{
+	char	*home;
+	char	*tmp;
+
+	if (!(home = get_env_var("HOME", env)))
+		return (NULL);
+	if (!arg)
+		return (home);
+	else
+	{
+		if ((tmp = ft_strjoin(home, arg + 1)))
+		{
+			*allocated = true;
+			return (tmp);
+		}
+		return (NULL);
+	}
+}
+
+int			ms_cd(int ac, char **av, t_env *env)
 {
 	char	*path;
-	t_bool	is_allocated;
+	t_bool	allocated;
 
 	av[ac] = NULL;
-	is_allocated = false;
-	if ((path = av[1]) == NULL)
-		path = get_env_var("HOME", env);
-	if (*path == '~')
-		if ((path = ft_strjoin(get_env_var("HOME", env), path + 1)))
-			is_allocated = true;
-	if (path == NULL || chdir(path) == -1)
+	allocated = false;
+	if (!(path = av[1]) || *path == '~')
+		path = set_path_with_home(av[1], env, &allocated);
+	if (path && chdir(path) == -1)
 	{
 		write(1, "cd: no such file or directory: ", 31);
 		write(1, av[1], ft_strlen(av[1]));
 		write(1, "\n", 1);
-		return (1);
+		return (EXIT_FAILURE);
 	}
-	if (path && is_allocated)
+	else if (!path)
+	{
+		write(1, "cd: HOME env variable not found\n", 32);
+		return (EXIT_FAILURE);
+	}
+	if (path && allocated)
 		free(path);
-	return (0);
+	return (EXIT_SUCCESS);
 }
