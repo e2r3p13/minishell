e4ce2c658276bd00e21f42f52da670dcf1330f32
Loïc Buckwell <lo.buckwell@gmail.com>
2020-04-06 13:58:17 +0200

    echo fix

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,13 +6,23 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 16:44:50 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/03/25 12:29:36 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/04/06 13:55:35 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int		set_flag(char *arg, int *f)
+static int		is_flag(char *s)
+{
+	if (!*s)
+		return (false);
+	while (*s)
+		if (!ft_isinset("n", *s++))
+			return (false);
+	return (true);
+}
+
+static void		set_flag(char *arg, int *f)
 {
 	arg++;
 
@@ -20,11 +30,8 @@ static int		set_flag(char *arg, int *f)
 	{
 		if (*arg == 'n')
 			*f |= 0b1000;
-		else
-			return (0);
 		arg++;
 	}
-	return (1);
 }
 
 int				ms_echo(int ac, char **av)
@@ -35,13 +42,8 @@ int				ms_echo(int ac, char **av)
 	i = 1;
 	f = 0b0000;
 	av[ac] = NULL;
-	while (i < ac && ft_strncmp(av[i], "-", 1) == 0)
-	{
-		if (set_flag(av[i], &f))
-			i++;
-		else
-			break;
-	}
+	while (i < ac && *av[i] == '-' && is_flag(av[i] + 1))
+		set_flag(av[i++], &f);
 	while (i < ac)
 	{
 		write(1, av[i], ft_strlen(av[i]));
