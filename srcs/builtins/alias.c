5e12ea656de6811b7769472621753e3fd7b14d5b
Loïc Buckwell <lo.buckwell@gmail.com>
2020-04-12 17:25:01 +0200

    protection

--- srcs/builtins/alias.c
+++ srcs/builtins/alias.c
@@ -6,7 +6,7 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/12 14:16:24 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/04/12 14:30:26 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/04/12 17:24:37 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ extern t_als	*g_als;
 
 int	ms_alias(int ac, char **av)
 {
-	if (av[1] && ft_strcmp(av[1], "minishell") == 0)
+	if ((av[1] && ft_strcmp(av[1], "minishell") == 0) || !g_als)
 		return (1);
 	return (ms_export(ac, av, g_als));
 }
