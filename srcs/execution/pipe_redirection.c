368c5dd3bd330f6bc6b4a6ae6416b434667c357b
Loïc Buckwell <lo.buckwell@gmail.com>
2020-04-16 16:55:12 +0200

    pipe redirection

--- srcs/execution/pipe_redirection.c
+++ srcs/execution/pipe_redirection.c
@@ -6,16 +6,45 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/14 20:13:35 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/04/16 12:17:15 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/04/16 16:53:28 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+static void	pipe_init(int *fd, int *pid)
+{
+	pid[0] = 0;
+	pid[1] = 0;
+	pipe(fd);
+}
 
 int			pipe_redirection(t_ast *ast, t_env *env)
 {
+	int		fd[2];
+    int		pid[2];
+	int		status;
+	int		fexit;
 
-	ast = NULL; env = NULL;
-	printf("execute pipe\n");
-	return (0);
+	pipe_init(fd, pid);
+    if ((pid[0] = fork()) == 0)
+    {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        exit(execute(ast->left, env));
+    }
+	if ((pid[1] = fork()) == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[1]);
+		exit(execute(ast->right, env));
+	}
+	fexit = waitpid(-1, &status, 0);
+	close(fexit == pid[1] ? fd[0] : fd[1]);
+	waitpid(fexit == pid[1] ? pid[0] : pid[1], &status, 0);
+    return (status);
 }
