d5bf0af8a62b317730bc284915d12fd0bb0f6725
Loïc Buckwell <lo.buckwell@gmail.com>
2020-04-30 17:00:06 +0200

    move should_continue into g_should_run

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,15 +6,19 @@
 /*   By: bccyv <bccyv@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 12:22:02 by bccyv             #+#    #+#             */
-/*   Updated: 2020/04/27 12:27:44 by bccyv            ###   ########.fr       */
+/*   Updated: 2020/04/30 16:59:21 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include "minishell.h"
+
+extern t_bool	g_should_run;
 
 int	ms_exit(void)
 {
-  write(1, "exit\n", 5);
-  return (EXIT_RETURN_VALUE);
+	write(1, "exit\n", 5);
+	g_should_run = false;
+	return (EXIT_SUCCESS);
 }
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 17:37:37 by lfalkau           #+#    #+#             */
-/*   Updated: 2020/04/30 16:38:19 by lfalkau          ###   ########.fr       */
+/*   Updated: 2020/04/30 16:54:45 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,17 @@
 ** Runs until EOI is reached (ctrl-D).
 */
 
-int	g_exitcode = 0;
+int		g_exitcode = 0;
+t_bool	g_should_run = true;
 
-int			minishell(t_env *env, t_hst **hst, t_bool it)
+int	minishell(t_env *env, t_hst **hst, t_bool it)
 {
 	char		*cmd;
 	t_lxr		**lxrlst;
 	t_ast		*ast;
 	int			i;
-	t_bool	should_continue;
 
-	should_continue = true;
-	while (should_continue)
+	while (g_should_run)
 	{
 		it ? prompt(env) : 1;
 		if (!(cmd = it ? get_it_cmd(hst) : get_cmd()))
@@ -51,8 +50,7 @@ int			minishell(t_env *env, t_hst **hst, t_bool it)
 			{
 				if ((ast = ast_create(lxrlst[i])))
 				{
-					if ((g_exitcode = execute(ast, env)) == EXIT_RETURN_VALUE)
-						should_continue = false;
+					g_exitcode = execute(ast, env);
 					ast_free(ast);
 				}
 				lxr_free(lxrlst[i]);
